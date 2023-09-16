a429f07eda005ce1b5d72d1524624a446504a636
Dominic Galiano <dgaliano@utexas.edu>
2023-09-16 13:45:23 -0400

    added timezone package and rudimentary support

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import dayjs, { Dayjs } from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 function App() {
   const [time, setTime] = useState<Dayjs>(dayjs());
 
-  // useEffect(() => {
-  //   setTime(dayjs());
-  // }, [time]);
+  useEffect(() => {
+    // Update time every second
+    const interval = setInterval(() => {
+      setTime(dayjs());
+    }, 1000);
+
+    // cleanup on component unmount
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="container">
-      <h2>Location</h2>
+      <h2>{dayjs.tz.guess()}</h2>
       <h1>{time.format("hh:mm:ss A")}</h1>
       <h2>{time.format("MMMM D, YYYY")}</h2>
     </div>
