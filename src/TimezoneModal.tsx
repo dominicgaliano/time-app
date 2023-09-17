b7f84a822a3ddf8346ece2332bb02bb5e3a2bebe
Dominic Galiano <dgaliano@utexas.edu>
2023-09-17 12:19:42 -0400

    refactored to use react-select for dropdown

--- src/TimezoneModal.tsx
+++ src/TimezoneModal.tsx
@@ -1,23 +1,27 @@
 import MicroModal from "react-micro-modal";
+import Select from "react-select";
 // timezones from file as Intl.supportedValuesOf('timeZone') does not work in react.
 import timezones from "./data/timezones.json";
 
 export default function TimezoneModal() {
+  const timezoneOptions = timezones.map((timezone) => {
+    return {
+      value: timezone,
+      label: timezone,
+    };
+  });
+
   return (
     <MicroModal
       trigger={(handleOpen) => (
         <button onClick={handleOpen}>Change Timezone</button>
       )}
       children={(handleClose) => (
-        <form>
-          <label htmlFor="timezones">Change Timezone</label>
-          <select id="timezones" name="timezones">
-            {timezones.map((timezone) => (
-              <option value={timezone}>{timezone}</option>
-            ))}
-          </select>
+        <div>
+          <label>Change Timezone</label>
+          <Select options={timezoneOptions} />
           <button onClick={handleClose}>Apply</button>
-        </form>
+        </div>
       )}
     />
   );
