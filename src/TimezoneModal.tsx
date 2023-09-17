d63551d3d95022616bc51e83a56a1e22aa9a16cf
Dominic Galiano <dgaliano@utexas.edu>
2023-09-17 11:57:24 -0400

    moved modal to own module and documented timezones

--- src/TimezoneModal.tsx
+++ src/TimezoneModal.tsx
@@ -1,4 +1,6 @@
 import MicroModal from "react-micro-modal";
+// timezones from file as Intl.supportedValuesOf('timeZone') does not work in react.
+import * as timezones from "./timezones.json";
 
 export default function TimezoneModal() {
   return (
@@ -6,7 +8,11 @@ export default function TimezoneModal() {
       trigger={(handleOpen) => (
         <button onClick={handleOpen}>Change Timezone</button>
       )}
-      children={(handleClose) => <button onClick={handleClose}>Close!</button>}
+      children={(handleClose) => (
+        <div>
+          <button onClick={handleClose}>Close!</button>
+        </div>
+      )}
     />
   );
 }
