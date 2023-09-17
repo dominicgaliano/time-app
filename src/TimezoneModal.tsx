b6e415f3739e8bd436de09d5d58b6be9b991f4ae
Dominic Galiano <dgaliano@utexas.edu>
2023-09-17 12:15:17 -0400

    path correction

--- src/TimezoneModal.tsx
+++ src/TimezoneModal.tsx
@@ -1,6 +1,6 @@
 import MicroModal from "react-micro-modal";
 // timezones from file as Intl.supportedValuesOf('timeZone') does not work in react.
-import timezones from ".data/timezones.json";
+import timezones from "./data/timezones.json";
 
 export default function TimezoneModal() {
   return (
