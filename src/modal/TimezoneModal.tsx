0136c5d46b635e4e466eb499c45cadc93822fdff
Dominic Galiano <dgaliano@utexas.edu>
2023-09-18 11:40:40 -0400

    added select placeholder

--- src/modal/TimezoneModal.tsx
+++ src/modal/TimezoneModal.tsx
@@ -59,6 +59,7 @@ export default function TimezoneModal({
             value={currentTimezone}
             options={timezoneOptions}
             onChange={onChange}
+            placeholder={currentTimezone}
           />
           <button onClick={handleClose}>Apply</button>
         </div>
